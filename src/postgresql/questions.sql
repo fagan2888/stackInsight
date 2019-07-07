047765095e2c88931442e1c40fa54c7773e4fe97
vvt221 <vata695@gmail.com>
2019-07-07 11:14:05 -0400

    table schema

--- src/postgresql/questions.sql
+++ src/postgresql/questions.sql
@@ -1,6 +1,7 @@
-CREATE TABLE qtable (
+CREATE TABLE questions (
  id serial primary key,
  qid integer,
- tags VARCHAR[],
+ tags VARCHAR(500) [],
  duration numeric(10,3),
- community varchar(200));
+ community varchar(200).
+ pr_score numeric(10,2));
