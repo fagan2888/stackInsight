3b44a68fc293dc66e1af3fa8ebb6552c0bba36b0
vvt221 <vata695@gmail.com>
2019-07-14 13:35:42 -0400

    adding main function and description

--- src/s3/urls_retrieve.py
+++ src/s3/urls_retrieve.py
@@ -3,24 +3,20 @@ import requests
 from multiprocessing import Pool
 import csv
 import time
+""" This script uses BeautifulSoup to parse the urls to the stackexchange file and store them in a text file
 
-def write_to_csv(list_files,file_name):
-   if len(list_files) >0:
-      with open(file_name + '.csv', 'w') as target:
-           writer = csv.writer(target)
-           for row in urls_list:
-             writer.writerow([row])
            
 
-url = 'https://archive.org/download/stackexchange'
-response = requests.get(url)
-page_content = BeautifulSoup(response.text)
-tabledatas = page_content.find_all("td")[1:]
-urls_list = []
-for tabledata in tabledatas:
-  if len(tabledata.find_all("a"))>0 :
-    if tabledata.find_all("a")[0]['href'][-3:] == '.7z':
-      urls_list.append('https://archive.org/download/stackexchange/'  + tabledata.find_all("a")[0]['href'])
-
-
-write_to_csv(urls_list,'/home/ubuntu/urls_list')
+if __name__ == '__main__':
+ url = 'https://archive.org/download/stackexchange'
+ response = requests.get(url)
+ page_content = BeautifulSoup(response.text)
+ tabledatas = page_content.find_all("td")[1:]
+ urls_list = []
+ for tabledata in tabledatas:
+   if len(tabledata.find_all("a"))>0 :
+     if tabledata.find_all("a")[0]['href'][-3:] == '.7z':
+       urls_list.append('https://archive.org/download/stackexchange/'  + tabledata.find_all("a")[0]['href'])
+ with open('urls_list.txt', 'w') as f:
+   for item in urls_list:
+       f.write("%s\n" % item)
