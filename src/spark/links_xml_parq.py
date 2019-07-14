9e71af3298e85d65c9e67fc4efc07634fd25d101
vvt221 <vata695@gmail.com>
2019-07-14 16:45:24 -0400

    Update links_xml_parq.py

--- src/spark/links_xml_parq.py
+++ src/spark/links_xml_parq.py
@@ -6,16 +6,7 @@ import pyspark.sql.functions as f
 import re,html,csv
 from boto3 import client
 import csv
-conn = client('s3')
 
-#set up a spark session:
-sc = SparkContext(conf=SparkConf().setAppName("se"))
-
-spark = SparkSession.builder.appName("se").getOrCreate()
-#preprocessing functions:
-pattern = re.compile(' ([A-Za-z]+)="([^"]*)"')
-parse_line = lambda line: {key:value for key,value in pattern.findall(line)}
-unescape = udf(lambda escaped: html.unescape(escaped) if escaped else None)
 
 def file_names_retrieve():
     conn = client('s3')
@@ -25,14 +16,17 @@ def file_names_retrieve():
        if "links" in file_name:
           links_file_names.append(file_name)
     return links_file_names
-
-link_files = file_names_retrieve()
-
-
-parquet_files = ["s3a://xmlsefiles/" + link_file for link_file in link_files ]
-for link_file in range(link_files):
-    
-    tdf = spark.read.text(kb_list_full[gz_file_id]).where(col('value').like('%<row Id%')) \
+#set up a spark session:
+if __name__ == "__main__":
+  sc = SparkContext(conf=SparkConf().setAppName("se"))
+  spark = SparkSession.builder.appName("se").getOrCreate()
+  #preprocessing functions:
+  pattern = re.compile(' ([A-Za-z]+)="([^"]*)"')
+  parse_line = lambda line: {key:value for key,value in pattern.findall(line)}
+  unescape = udf(lambda escaped: html.unescape(escaped) if escaped else None)
+  link_file_names = file_names_retrieve()
+  link_files = ["s3a://xmlsefiles/" + link_file_name for link_file_name in link_file_names]
+  tdf = spark.read.text(link_files).where(col('value').like('%<row Id%')) \
     .select(udf(parse_line, MapType(StringType(), StringType()))('value').alias('value')) \
     .select(
         col('value.Id').cast('integer'),
@@ -41,20 +35,12 @@ for link_file in range(link_files):
         col('value.RelatedPostId').cast('integer'),
         col('value.LinkTypeId').cast('integer'),
     )
-
-    #read all the Community Names through the filenames from the xml files:
-    fdf = spark.read.text(kb_list_full[gz_file_id]).where(col('value').like('%<row Id%')).select(input_file_name())
-   
-    clean_name_df = fdf.withColumn('Community', regexp_extract(col('input_file_name()'), './(links)([\w\.]+)',2))
-    clean_name_df_subset = clean_name_df.select("Community")
-    clean_name_df_subset.show(5,truncate=False)
-    #establish id as the common key between the two dataframes created above
-    tdf_id = tdf.withColumn("iid", monotonically_increasing_id())
-    fdf_id = clean_name_df_subset.withColumn("iid", monotonically_increasing_id())
-
-    #join the two dataframes on "id" and remove id
-    
-    community_posts = tdf_id.join(fdf_id, tdf_id.iid == fdf_id.iid).drop("iid")
-    community_posts.write.parquet('s3a://parquetoutputse/'+ link_file +'.parquet')
-
-    spark.catalog.clearCache()
+  fdf = spark.read.text(link_files).where(col('value').like('%<row Id%')).select(input_file_name())
+  clean_name_df = fdf.withColumn('Community', regexp_extract(col('input_file_name()'), './(links)([\w\.]+)',2))
+  clean_name_df_subset = clean_name_df.select("Community")
+  #establish id as the common key between the two dataframes created above
+  tdf_id = tdf.withColumn("iid", monotonically_increasing_id())
+  fdf_id = clean_name_df_subset.withColumn("iid", monotonically_increasing_id())
+  community_links = tdf_id.join(fdf_id, tdf_id.iid == fdf_id.iid).drop("iid")
+  community_links.write.parquet('s3a://xmlparq/links.parquet')
+  spark.catalog.clearCache()
