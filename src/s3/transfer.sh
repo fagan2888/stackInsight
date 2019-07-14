07c393e244628d54456067915cb3d41f020b13fd
vvt221 <vvt221@nyu.edu>
2019-07-14 00:07:06 -0400

    script to transfer files to s3

---
+++ src/s3/transfer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script is used to redirect the archives in the urls of the csv file to the amazon s3 bucket
+
+input=$HOME'/urls_subset_list.csv'
+echo $input
+while read url
+do
+    
+
+    echo $url
+    echo "Transferring to S3"
+    file_name=$(echo $url | rev | cut -d '/' -f 1 | rev)
+    echo $file_name
+    wget -qO- $url | aws s3 cp - s3://sedatadump/$file_name
+
+    echo "Transfer Complete"
+done < "$input"
+
