805f98ea1cf6e13023ca9e77a91b0a675d37aa4e
vvt221 <vata695@gmail.com>
2019-07-15 08:43:01 -0400

    Update de_dag.py

--- src/airflow/de_dag.py
+++ src/airflow/de_dag.py
@@ -75,7 +75,7 @@ cmd_pr_spark = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
    --executor-memory 20G \
    --total-executor-cores 4 \
    --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
-   --packages graphframes:graphframes:0.5.0-spark2.1-s_2.1
+   --packages graphframes:graphframes:0.5.0-spark2.1-s_2.1 \
    /home/ubuntu/stackInsights/spark/pagerank_calculation.py"
    
 pr_calculation = BashOperator(
