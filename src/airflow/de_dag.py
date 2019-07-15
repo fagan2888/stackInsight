6a8db2c518a4c447ed401fd5114d8a05f8881a66
vvt221 <vata695@gmail.com>
2019-07-15 08:26:50 -0400

    Update de_dag.py

--- src/airflow/de_dag.py
+++ src/airflow/de_dag.py
@@ -39,7 +39,7 @@ cmd_posts_pr = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
    --deploy-mode cluster \
    --executor-memory 20G \
    --driver-memory 8G \
-   --total-executor-cores 6 \
+   --total-executor-cores 4 \
    --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
    /home/ubuntu/stackInsights/spark/posts_xml_parq.py"
 
@@ -56,7 +56,7 @@ cmd_posts_pr = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
    --deploy-mode cluster \
    --executor-memory 20G \
    --driver-memory 8G \
-   --total-executor-cores 6 \
+   --total-executor-cores 4 \
    --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
    /home/ubuntu/stackInsights/spark/links_xml_parq.py"
 
@@ -73,8 +73,7 @@ cmd_pr_spark = "ssh ubuntu@10.0.0.13 /usr/local/spark/bin/spark-submit \
    --master spark://ip-10-0-0-13:7077 \
    --deploy-mode cluster \
    --executor-memory 20G \
-   --driver-memory 8G \
-   --total-executor-cores 6 \
+   --total-executor-cores 4 \
    --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
    --packages graphframes:graphframes:0.5.0-spark2.1-s_2.1
    /home/ubuntu/stackInsights/spark/pagerank_calculation.py"
