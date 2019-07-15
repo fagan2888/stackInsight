30d041dc230cb4dce870eaeb6668be59f52ba4bc
vvt221 <vata695@gmail.com>
2019-07-15 08:37:06 -0400

    Create df_join.sh

---
+++ df_join.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+export PYSPARK_PYTHON=python3
+/usr/local/spark/bin/spark-submit \
+   --master spark://ip-10-0-0-13:7077 \
+   --deploy-mode cluster \
+   --executor-memory 20G \
+   --driver-memory 8G \
+   --total-executor-cores 6 \
+   --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
+   /home/ubuntu/stackInsights/spark/join_dfs.py
