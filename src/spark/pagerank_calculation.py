0f6d7463c565c63b371ecfaed08d1291c28b6f8e
vvt221 <vata695@gmail.com>
2019-07-15 00:02:48 -0400

    Update pagerank_calculation.py

--- src/spark/pagerank_calculation.py
+++ src/spark/pagerank_calculation.py
@@ -10,8 +10,7 @@ import re,html,csv
 from graphframes import *
 #set up a spark session:
 
-if __name__ == "__main__":
-  
+def main():
   sc = SparkContext(conf=SparkConf().setAppName("se"))
   spark = SparkSession.builder.appName("se").getOrCreate()
   df3 = spark.read.load("s3a://xmlparq/links.parquet")
@@ -38,3 +37,36 @@ if __name__ == "__main__":
   pr_df = pr_df.withColumnRenamed("pagerank","cred_score")
   pr_df = pr_df.withColumn("cred_score", f.round(pr_df["cred_score"], 5))
   pr_df.write.parquet("s3a://xmlparq/pr_se_links.parquet")
+
+  sc = SparkContext(conf=SparkConf().setAppName("se"))
+  spark = SparkSession.builder.appName("se").getOrCreate()
+  df3 = spark.read.load("s3a://xmlparq/links.parquet")
+
+  #find only those links which are not duplicate:
+  links = df3.filter((f.col('LinkTypeId')==1))
+  #Extract only those columns which are required for our analysis:
+  partitioneddf = df3.repartition("Community")
+  links_df_subset = partitioneddf.select('Id', 'CreationDate', 'RelatedPostId','Community')
+  #Rename the columns 
+  new_column_names = ['src', 'CreationDate' ,'dst','community']
+  edges = links_df_subset.toDF(*new_column_names)
+  v1 = links.select('Id','community','CreationDate')
+  v2 = links.select('RelatedPostId','community','CreationDate')
+  v1 = v1.select('Id','community').distinct()
+  v2 = v2.select('RelatedPostId','community').distinct()
+  v1 = v1.withColumnRenamed("Id","id")
+  v2 = v2.withColumnRenamed("RelatedPostId","id")
+  vertices = v1.union(v2)
+  graph_frame = GraphFrame(vertices,e)
+  #perform pagerank to identify pages with high rank
+  pr = graph_frame.pageRank(resetProbability=0.15, tol=0.01)
+  pr_df = pr.vertices.sort(['pagerank'],ascending=[0])
+  pr_df = pr_df.withColumnRenamed("pagerank","cred_score")
+  pr_df = pr_df.withColumn("cred_score", f.round(pr_df["cred_score"], 5))
+  pr_df.write.parquet("s3a://xmlparq/pr_se_links.parquet")
+
+  
+
+if __name__ == "__main__":
+  
+  main()
